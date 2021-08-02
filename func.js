151019440f86a12350c29a2aa752f8780cc7f658
Emina Ergul <eminaergul712@gmail.com>
2021-08-02 14:08:22 +0100

    gallery functioning

--- func.js
+++ func.js
@@ -12,3 +12,26 @@ nav.style.transitionDelay = "0.5s"
   }
   prevScrollPos = currentScrollPos;
 }
+
+// image gallery
+const cover1 = document.querySelector(".cover1")
+const cover2 = document.querySelector(".cover2")
+const btnLeft = document.querySelector(".left")
+const btnRight = document.querySelector(".right")
+let slideCount = 1
+
+const changeSlide = () => {
+  if(slideCount === 1) {
+    cover1.style.display = "none"
+    cover2.style.display = "flex"
+    slideCount = 2
+  } else if (slideCount === 2) {
+    cover1.style.display = "flex"
+    cover2.style.display = "none"
+    slideCount = 1
+  }
+}
+
+btnRight.addEventListener("click", changeSlide)
+btnLeft.addEventListener("click", changeSlide)
+
