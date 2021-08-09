694acb666d4ca5d2c1542ee4b98e1604ec6b78fa
Emina Ergul <eminaergul712@gmail.com>
2021-08-09 11:48:59 +0100

    added expandable btns in side nav

--- func.js
+++ func.js
@@ -48,11 +48,18 @@ for(var i = 0; i < dots.length; i++) {
 const btnMenu = document.querySelector(".menu-btn")
 const sideNav = document.querySelector(".side-nav")
 const touchNav = document.querySelector(".touch-nav")
+const btnGames = document.querySelector(".game-dropdown")
+const expandableG = document.querySelector(".expandable-g-container")
+const gChev = document.querySelector(".g-chevdown")
+const btnHardware = document.querySelector(".hardware-dropdown")
+const expandableH = document.querySelector(".expandable-h")
+const hChev = document.querySelector(".h-chevdown")
 let menuOpen = false
+let gamesOpen = false
+let hardwareOpen = false
 
 btnMenu.addEventListener("click", () => {
   if(!menuOpen) {
-    console.log("open")
     btnMenu.classList.add("open")
     sideNav.style.transform = "translateX(-300px)"
     touchNav.style.transform = "translateY(70px)"
@@ -62,6 +69,34 @@ btnMenu.addEventListener("click", () => {
     sideNav.style.transform = "translateX(300px)"
     touchNav.style.transform = "translateY(0px)"
     menuOpen = false
+  }
+})
+
+btnGames.addEventListener("click", () => {
+  if(!gamesOpen) {
+    gamesOpen = true
+    expandableG.style.display = "flex"
+    gChev.classList.remove("fa-chevron-down")
+    gChev.classList.add("fa-chevron-up")
+  } else {
+    gamesOpen = false
+    expandableG.style.display = "none"
+    gChev.classList.remove("fa-chevron-up")
+    gChev.classList.add("fa-chevron-down")
+  }
+})
+
+btnHardware.addEventListener("click", () => {
+  if(!hardwareOpen) {
+    hardwareOpen = true
+    expandableH.style.display = "flex"
+    hChev.classList.remove("fa-chevron-down")
+    hChev.classList.add("fa-chevron-up")
+  } else {
+    hardwareOpen = false
+    expandableH.style.display = "none"
+    hChev.classList.remove("fa-chevron-up")
+    hChev.classList.add("fa-chevron-down")
   }
 })
 
